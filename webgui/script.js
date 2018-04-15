6ec725182effe3734832c61fc952a63d2d801303
Alexander Taylor <alexanderjohntaylor@gmail.com>
2018-04-15 01:24:56 +0100

    Added market cards to preliminary gui

--- webgui/script.js
+++ webgui/script.js
@@ -1,6 +1,133 @@
+class Card {
+    constructor(tier, colour, points,
+                white=0, blue=0, green=0, red=0, black=0) {
+        this.tier = tier;
+        this.colour = colour;
+        this.points = points;
+
+        this.gems = {
+            white: white,
+            blue: blue,
+            green: green,
+            red: red,
+            black: black};
+    }
+}
+
+var tier_1 = [
+    new Card(1, 'blue', 0, black=3),
+    new Card(1, 'blue', 0, white=1, black=2),
+    new Card(1, 'blue', 0, green=2, black=2),
+    new Card(1, 'blue', 0, white=1, green=2, red=2),
+    new Card(1, 'blue', 0, blue=1, green=3, red=1),
+    new Card(1, 'blue', 0, white=1, green=1, red=1, black=1),
+    new Card(1, 'blue', 0, white=1, green=1, red=2, black=1),
+    new Card(1, 'blue', 1, red=4),
+
+    new Card(1, 'red', 0, white=3),
+    new Card(1, 'red', 0, blue=2, green=1),
+    new Card(1, 'red', 0, white=2, red=2),
+    new Card(1, 'red', 0, white=2, green=1, black=2),
+    new Card(1, 'red', 0, white=1, red=1, black=3),
+    new Card(1, 'red', 0, white=1, blue=1, green=1, black=1),
+    new Card(1, 'red', 0, white=2, blue=1, green=1, black=1),
+    new Card(1, 'red', 1, white=4),
+
+    new Card(1, 'black', 0, green=3),
+    new Card(1, 'black', 0, green=2, red=1),
+    new Card(1, 'black', 0, white=2, green=2),
+    new Card(1, 'black', 0, white=2, blue=2, red=1),
+    new Card(1, 'black', 0, green=1, red=3, black=1),
+    new Card(1, 'black', 0, white=1, blue=1, green=1, red=1),
+    new Card(1, 'black', 0, white=1, blue=2, green=1, red=1),
+    new Card(1, 'black', 1, blue=4),
+
+    new Card(1, 'white', 0, blue=3),
+    new Card(1, 'white', 0, red=2, black=1),
+    new Card(1, 'white', 0, blue=2, black=2),
+    new Card(1, 'white', 0, blue=2, green=2, black=1),
+    new Card(1, 'white', 0, white=3, blue=1, black=1),
+    new Card(1, 'white', 0, blue=1, green=1, red=1, black=1),
+    new Card(1, 'white', 0, blue=1, green=2, red=1, black=1),
+    new Card(1, 'white', 1, green=4),
+
+    new Card(1, 'green', 0, red=3),
+    new Card(1, 'green', 0, white=2, blue=1),
+    new Card(1, 'green', 0, blue=2, red=2),
+    new Card(1, 'green', 0, blue=1, red=2, black=2),
+    new Card(1, 'green', 0, white=1, blue=3, green=1),
+    new Card(1, 'green', 0, white=1, blue=1, red=1, black=1),
+    new Card(1, 'green', 0, white=1, blue=1, red=1, black=2),
+    new Card(1, 'green', 1, black=4)
+];
+
+var tier_2 = [
+    new Card(2, 'blue', 1, blue=2, green=2, red=3),
+    new Card(2, 'blue', 1, blue=2, green=3, black=3),
+    new Card(2, 'blue', 2, blue=5),
+    new Card(2, 'blue', 2, white=5, blue=3),
+    new Card(2, 'blue', 2, white=2, red=1, black=4),
+    new Card(2, 'blue', 3, blue=6),
+
+    new Card(2, 'red', 1, white=2, red=2, black=3),
+    new Card(2, 'red', 1, blue=3, red=2, black=3),
+    new Card(2, 'red', 2, black=5),
+    new Card(2, 'red', 2, white=3, black=5),
+    new Card(2, 'red', 2, white=1, blue=4, green=2),
+    new Card(2, 'red', 3, red=6),
+
+    new Card(2, 'black', 1, white=3, blue=2, green=2),
+    new Card(2, 'black', 1, white=3, green=3, black=2),
+    new Card(2, 'black', 2, white=5),
+    new Card(2, 'black', 2, green=5, red=3),
+    new Card(2, 'black', 2, blue=1, green=4, red=2),
+    new Card(2, 'black', 3, black=6),
+
+    new Card(2, 'white', 1, green=3, red=2, black=2),
+    new Card(2, 'white', 1, white=2, blue=3, red=3),
+    new Card(2, 'white', 2, red=5),
+    new Card(2, 'white', 2, red=5, black=3),
+    new Card(2, 'white', 2, green=1, red=4, black=2),
+    new Card(2, 'white', 3, white=6),
+
+    new Card(2, 'green', 1, white=2, blue=3, black=2),
+    new Card(2, 'green', 1, white=3, green=2, red=3),
+    new Card(2, 'green', 2, green=5),
+    new Card(2, 'green', 2, blue=5, green=3),
+    new Card(2, 'green', 2, white=4, blue=2, black=1),
+    new Card(2, 'green', 3, green=6)
+];
+
+var tier_3 = [
+    new Card(3, 'blue', 3, white=3, green=3, red=3, black=5),
+    new Card(3, 'blue', 4, white=7),
+    new Card(3, 'blue', 4, white=6, blue=3, black=3),
+    new Card(3, 'blue', 5, white=7, blue=3),
+
+    new Card(3, 'red', 3, white=3, blue=5, green=3, black=5),
+    new Card(3, 'red', 4, green=7),
+    new Card(3, 'red', 4, blue=3, green=6, red=3),
+    new Card(3, 'red', 5, green=7, red=3),
+
+    new Card(3, 'black', 3, white=3, blue=3, green=5, red=3),
+    new Card(3, 'black', 4, red=7),
+    new Card(3, 'black', 4, green=3, red=6, black=3),
+    new Card(3, 'black', 5, red=7, black=3),
+
+    new Card(3, 'white', 3, blue=3, green=3, red=5, black=3),
+    new Card(3, 'white', 4, black=7),
+    new Card(3, 'white', 4, white=3, red=3, black=6),
+    new Card(3, 'white', 5, white=3, black=7),
+
+    new Card(3, 'green', 3, white=5, blue=3, red=3, black=3),
+    new Card(3, 'green', 4, blue=7),
+    new Card(3, 'green', 4, white=3, blue=6, green=3),
+    new Card(3, 'green', 5, blue=7, green=3)
+];
 
 class Player {
-    constructor() {
+    constructor(number) {
+        this.number = number;
         this.cards_in_hand = [];
         this.cards_played = [];
         this.nobles = [];
@@ -42,15 +169,46 @@ class GameState {
                             gold: this.num_gold_gems_in_play};
 
         for (var i = 0; i < this.num_players; i++) {
-            this.players.push(new Player());
+            this.players.push(new Player(i + 1));
         }
+
+        this.tier_1 = tier_1.slice();
+        this.tier_1_visible = [];
+        this.tier_2 = tier_2.slice();
+        this.tier_2_visible = [];
+        this.tier_3 = tier_3.slice();
+        this.tier_3_visible = [];
         
+        this.refill_market();
+    }
+
+    refill_market() {
+        while (this.tier_1_visible.length < 4 &&
+               this.tier_1.length > 0) {
+            var card = this.tier_1.pop();
+            this.tier_1_visible.push(card);
+        }
+
+        while (this.tier_2_visible.length < 4 &&
+               this.tier_2.length > 0) {
+            var card = this.tier_2.pop();
+            this.tier_2_visible.push(card);
+        }
+
+        while (this.tier_3_visible.length < 4 &&
+               this.tier_3.length > 0) {
+            var card = this.tier_3.pop();
+            this.tier_3_visible.push(card);
+        }
     }
 
     reduce_gems() {
         this.supply_gems['green'] -= 2;
         this.supply_gems['blue'] += 1;
         this.players[0].score += 1;
+
+        this.tier_1_visible.pop();
+        this.refill_market();
     }
 }
 
@@ -58,16 +216,19 @@ var test_state = new GameState();
 
 
 Vue.component('gems-list', {
-    props: ['gems', 'title'],
+    props: {gems: {},
+            title: "",
+            display_zeros: {default: true}},
     template: `
-<div class="gem-counter">
+<div class="gems-list">
     <h3>{{ title }}</h3>
     <ul>
     <gem-counter 
         v-for="(number, colour) in gems"
         v-bind:key="colour"
         v-bind:colour="colour"
-        v-bind:number="number">
+        v-bind:number="number"
+        v-if="number > 0 || display_zeros">
     </gem-counter>
     </ul>
 </div>`
@@ -84,15 +245,49 @@ Vue.component('gem-counter', {
 Vue.component('player-display', {
     props: ['player'],
     template: `
-<p class="player-display">{{ player }}</p>
+<div class="player-display">
+<h3>Player {{ player.number }}</h3>
+    <p>score = {{ player.score }}</p>
+        <gems-list v-bind:gems="player.gems"
+                   title="player gems">
+        </gems-list>
+</div>
+`
+})
+
+Vue.component('market-display', {
+    props: ['cards', 'name'],
+    template: `
+<div class="market-display">
+    <h3>{{ name }}</h3>
+    <card-display
+        v-for="card in cards"
+        v-bind:key="card.id"
+        v-bind:card="card" >
+    </card-display>
+</div>
+`
+})
+
+Vue.component('card-display', {
+    props: ['card'],
+    template: `
+<div class="card-display">
+    <p>{{ card.colour }} card worth {{ card.points }} points</p>
+    <gems-list v-bind:gems="card.gems" 
+               v-bind:display_zeros="false">
+    </gems-list>
+</div>
 `
 })
 
 var app = new Vue({
     el: '#app',
     data: {
+        state: test_state,
         supply_gems: test_state.supply_gems,
         players: test_state.players
+
     },
     methods: {
         testChangeGems: function() {
